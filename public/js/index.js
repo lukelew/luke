7c0be99a20fce6a43ce09b7fe8e0e0d982213ff9
lukelew <ailuqun313@gmail.com>
2016-07-26 03:24:12 +0800

    works模块显示隐藏

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,47 @@
 var windowHeight = window.document.documentElement.clientHeight;
-console.log(windowHeight);
+document.getElementById('stage1_intro').style.height = windowHeight + 'px';
 
-// document.getElementById('stage1_intro').height(windowHeight);
-document.querySelector('#stage1_intro').style.height = windowHeight + 'px';
+var isSupportTouch = "ontouchend" in document ? true : false;
+//桌面端添加作品事件
+
+if(!isSupportTouch){
+  
+  var single = document.getElementsByClassName('single_work');
+}
+else{
+  console.log('mobile')
+}
+
+var app = angular.module('luke',[]);
+app.controller('works',function($scope){
+  $scope.indexPic = {
+    uniqueway_thumb: 'uniqueway-web.jpg',
+    hemax_thumb: 'hemax-web.jpg',
+    yidianer_thumb: 'yidianer-web.jpg',
+    patent_thumb: 'patent-web.jpg',
+    zhiyi_thumb: 'zhiyi-web.jpg'
+  }
+
+  $scope.toggleClass = function(e){
+    var myClassList = e.classList;
+    if(myClassList.contains('active')){
+      myClassList.remove('active')
+    }
+    else{
+      myClassList.add('active');
+    }
+  };
+
+
+  $scope.showBox = function(){
+    var popBox = document.getElementById('pop_box');
+    var overlay = document.getElementById('overlay');
+
+    $scope.toggleClass(popBox);
+    $scope.toggleClass(overlay);
+  }
+
+  $scope.insertPic = function(e){
+    $scope.replacePic = $scope.indexPic[e];
+  }
+})
