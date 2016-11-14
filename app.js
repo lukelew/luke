928a95b41a1dfbe368670fe0f8e995048ca0f687
lukelew <ailuqun313@gmail.com>
2016-11-15 03:05:27 +0800

    update the loading effect

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// app.listen(80);
+app.listen(8080);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
